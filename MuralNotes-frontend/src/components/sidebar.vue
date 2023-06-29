dc7419fb174ad2dd9e49ce0c81aeae16ec2ba199
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-06-28 23:53:37 -0400

    add card editor

--- MuralNotes-frontend/src/components/sidebar.vue
+++ MuralNotes-frontend/src/components/sidebar.vue
@@ -1,19 +1,36 @@
 <template>
 <StyledSidebarContainer>
+  <StyledInputContainer>
+    <n-input type="text" placeholder="search"></n-input>
+  </StyledInputContainer>
+  <StyledTagsContainer>
   <n-collapse default-expanded-names="alltags">
     <n-collapse-item title="All tags" name="alltags">
-      <div># memos</div>
-      <div># aws</div>
+      <StyledTagItem><div># memos</div></StyledTagItem>
+      <StyledTagItem><div># aws</div></StyledTagItem>
     </n-collapse-item>
   </n-collapse>
+  </StyledTagsContainer>
 </StyledSidebarContainer>
 </template>
 <script setup lang="ts">
 import styled from "vue3-styled-components";
-import { NCollapse, NCollapseItem } from "naive-ui";
+import { NCollapse, NCollapseItem, NInput } from "naive-ui";
 
 const StyledSidebarContainer = styled.div`
   flex-grow: 0;
   width: 300px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 15px;
+`;
+const StyledInputContainer = styled.div`
+  margin: 5px 10px;
+`;
+const StyledTagsContainer = styled.div`
+  margin: 5px 10px;
+`;
+const StyledTagItem = styled.div`
+  padding: 2px 10px 2px 20px;
 `;
 </script>
--- MuralNotes-frontend/src/components/workarea.vue
+++ MuralNotes-frontend/src/components/workarea.vue
@@ -1,23 +1,33 @@
 <template>
 <StyledWorkContainer>
-  <a href="https://vitejs.dev" target="_blank">
-    <img src="/vite.svg" class="logo" alt="Vite logo" />
-  </a>
-  <a href="https://vuejs.org/" target="_blank">
-    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-  </a>
-  <HelloWorld msg="Vite + Vue" />
+  <Editor
+   api-key="ofajqdu0x6g64n4wbwj2nbcfeu44iymvrd3kg59bj7mfocts"
+   :init="{
+        plugins: 'lists link image table code help wordcount',
+        toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | table | image | link',
+        menubar: false,
+        setup: (editor: any) => {
+          editor.on('Change', (e:any) => (onContentChange(e, editor)));
+        }
+      }"
+    />
 </StyledWorkContainer>
 </template>
 <script setup lang="ts">
 import styled from "vue3-styled-components";
+import Editor from "@tinymce/tinymce-vue";
 
-import HelloWorld from "./HelloWorld.vue";
 
 const StyledWorkContainer = styled.div`
   flex-grow: 10;
   min-width: 300px;
   max-width: 800px;
 `;
+
+function onContentChange(_event: any, editor: any) {
+  console.log("content changed");
+  const cnt = editor.getContent();
+  console.log(`content: ${cnt}`);
+}
 
 </script>
